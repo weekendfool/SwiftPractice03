0b0b9f712e77a349c7460cab0032f7b28fffa1ce
Ohara <nori.tukareta@gmail.com>
2020-07-07 13:36:07 +0900

    textFieldでpicker入力実装

--- TextFieldPickerSampleApp/TextFieldPickerSampleApp/ViewController.swift
+++ TextFieldPickerSampleApp/TextFieldPickerSampleApp/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        createPickerView()
     }
 
     //pickerViewの設定
