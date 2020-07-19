fc07e3c625c43a332c9e079e1ce51a9c8b6a7587
Ohara <nori.tukareta@gmail.com>
2020-07-19 16:45:18 +0900

    coredataの実験

--- CoreDataSampleApp/CoreDataSampleApp/ViewController.swift
+++ CoreDataSampleApp/CoreDataSampleApp/ViewController.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDiffableDataSource {
+    //パーツの紐付け
+    @IBOutlet weak var sampleTextField: UITextField!
+    @IBOutlet weak var sampleTableView: UITableView!
+    
+    //monsterEntityから引っ張ってきたデータを格納する配列
+    var monsterArray:[Monster] = []
+    
+    var managerOfContext = (UIApplication.shared.delegate as! AppDelegate).percistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        sampleTextField.delegate = self
+        sampleTableView.delegate = self
+        sampleTableView.dataSource = self
     }
+    
+    let myConditions = NSFetchRequest<NSFetchRequestResult>(entityName: "Monster")
 
 
 }
