aca229fa7b3edabebc2c39bc6acd7dceaeb921b7
Ohara <nori.tukareta@gmail.com>
2020-08-15 00:38:18 +0900

    数字以外入力不可にする処理実装

--- textFieldInputNumberSampleApp/textFieldInputNumberSampleApp/ViewController.swift
+++ textFieldInputNumberSampleApp/textFieldInputNumberSampleApp/ViewController.swift
@@ -16,8 +16,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        sampleTextField.delegate = self
     }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return string.isEmpty || (string.range(of: "^[0-9]+$", options: .regularExpression, range: nil, locale: nil) != nil)
+    }
+    
+    
 
     @IBOutlet var buttonAction: UIView!
     
