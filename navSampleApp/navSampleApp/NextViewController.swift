fa1459d02bae5d2e6f3a69ad7994c099db7a5004
Ohara <nori.tukareta@gmail.com>
2020-08-26 23:22:20 +0900

    画面遷移の実験アプリ作成

---
+++ navSampleApp/navSampleApp/NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  navSampleApp
+//
+//  Created by 尾原徳泰 on 2020/08/26.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ navSampleApp/navSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  navSampleApp
+//
+//  Created by 尾原徳泰 on 2020/08/26.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
