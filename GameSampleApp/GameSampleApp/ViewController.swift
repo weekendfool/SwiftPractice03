eda178c180340aa376f8f08546404e78bac898f0
Ohara <nori.tukareta@gmail.com>
2020-08-30 16:27:10 +0900

    gameSampleAppの画面作成及び紐付け

---
+++ GameSampleApp/GameSampleApp/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  GameSampleApp
+//
+//  Created by 尾原徳泰 on 2020/08/30.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sampleLabel: UILabel!
+    @IBOutlet weak var sampleButton1: UIButton!
+    @IBOutlet weak var sampleButton2: UIButton!
+    @IBOutlet weak var sampleButton3: UIButton!
+    @IBOutlet weak var sampleButton4: UIButton!
+    @IBOutlet weak var sampleButton5: UIButton!
+    @IBOutlet weak var sampleButton6: UIButton!
+    @IBOutlet weak var sampleButton7: UIButton!
+    @IBOutlet weak var sampleButton8: UIButton!
+    @IBOutlet weak var sampleButton9: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func sampleButton1Action(_ sender: Any) {
+    }
+    
+    @IBAction func sampleButton2Action(_ sender: Any) {
+    }
+    @IBAction func sampleButton3Action(_ sender: Any) {
+    }
+    
+    @IBAction func sampleButton4Action(_ sender: Any) {
+    }
+    
+    @IBAction func sampleButton5Action(_ sender: Any) {
+    }
+    
+    @IBAction func sampleButton6Action(_ sender: Any) {
+    }
+    
+    @IBAction func sampleButton7Action(_ sender: Any) {
+    }
+    
+    @IBAction func sampleButton8Action(_ sender: Any) {
+    }
+    
+    @IBAction func sampleButton9Action(_ sender: Any) {
+    }
+}
+
