ab23ecf803cfcce10e96b7ce66aa9e372bdde304
Ohara <nori.tukareta@gmail.com>
2020-09-02 01:09:43 +0900

    奇数と偶数で色を変える処理実装

--- GameSampleApp/GameSampleApp/ViewController.swift
+++ GameSampleApp/GameSampleApp/ViewController.swift
@@ -21,11 +21,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var sampleButton8: UIButton!
     @IBOutlet weak var sampleButton9: UIButton!
     
+    //プレイヤーの変数
+    var player = 0
+    var playerCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    func changeCouler(number: Int) {
+//MARK: - ボタンの色変え
+    func changeCouler(number: Int, player: Int) {
         var place:UIButton?
         //どのボタンが押されたかの条件分岐
         switch number {
@@ -50,36 +55,76 @@ class ViewController: UIViewController {
         default:
             return
         }
-        place?.backgroundColor = UIColor.red
+        if player == 1 {
+            //ボタンの色を変える処理
+            place?.backgroundColor = UIColor.red
+        } else if player == 2 {
+            //ボタンの色を変える処理
+            place?.backgroundColor = UIColor.blue
+        }
+        
+        //二回以上選択できなくする処理
+        place?.isEnabled = false
+        
         
     }
+    //MARK: - プレイヤーの選択
+    func judgmentPlayerNumber(playerCount: Int) {
+        //奇数か偶数かで１pか２pかを判断する
+        if (playerCount % 2) == 1 {
+            //奇数なら１p
+            player = 1
+        } else {
+            //偶数なら２p
+            player = 2
+        }
+        
+    }
+    //MARK: - ボタンのActionを関数化
+    func butttonActionGroup(number: Int) {
+        //プレイヤーを選別
+        judgmentPlayerNumber(playerCount: number)
+        //色を変える処理
+        changeCouler(number: number, player: player)
+        //playerCountのカウントをすすめる
+        playerCount += 1
+    }
+    
+    //MARK: - ボタンのAction
 
     @IBAction func sampleButton1Action(_ sender: Any) {
-        changeCouler(number: 1)
-        print(1)
+        butttonActionGroup(number: 1)
     }
     
     @IBAction func sampleButton2Action(_ sender: Any) {
+        butttonActionGroup(number: 2)
     }
     @IBAction func sampleButton3Action(_ sender: Any) {
+        butttonActionGroup(number: 3)
     }
     
     @IBAction func sampleButton4Action(_ sender: Any) {
+        butttonActionGroup(number: 4)
     }
     
     @IBAction func sampleButton5Action(_ sender: Any) {
+        butttonActionGroup(number: 5)
     }
     
     @IBAction func sampleButton6Action(_ sender: Any) {
+        butttonActionGroup(number: 6)
     }
     
     @IBAction func sampleButton7Action(_ sender: Any) {
+        butttonActionGroup(number: 7)
     }
     
     @IBAction func sampleButton8Action(_ sender: Any) {
+        butttonActionGroup(number: 8)
     }
     
     @IBAction func sampleButton9Action(_ sender: Any) {
+        butttonActionGroup(number: 9)
     }
 }
 
