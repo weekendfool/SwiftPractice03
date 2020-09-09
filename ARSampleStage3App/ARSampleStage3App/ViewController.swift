87171e5e07050194bc185593bea36444fea2949c
Ohara <nori.tukareta@gmail.com>
2020-09-09 19:51:02 +0900

    カメラ座標の中心点を指定、表示は失敗

--- ARSampleStage3App/ARSampleStage3App/ViewController.swift
+++ ARSampleStage3App/ARSampleStage3App/ViewController.swift
@@ -36,6 +36,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     // MARK: - カメラ座標の中心座標を記録する
 
     @IBAction func tappedButton(_ sender: Any) {
+        // 座標を決定しておく
+        //　カメラ座標系で３０センチ前に
+        let infrontOfCamera = SCNVector3(x: 0, y: 0, z: -0.3)
+        //  カメラ座標系からワールド座標系に変換
+        guard let cameraNode = sceneView.pointOfView else { return }
+        print("-----------------------------------------------")
+        print("cameraNode.camera!.zNear\(cameraNode.camera!.zNear)")
+        print("cameraNode.camera!.zFar\(cameraNode.camera!.zFar)")
+        let pointInWorld = cameraNode.convertPosition(infrontOfCamera, to: nil)
+        // ワールド座標からスクリーン座標へ変換
+        var screenPos = sceneView.projectPoint(pointInWorld)
+        print("screenPos\(screenPos)")
+        // scnファイルからシーンを読み込む
+        let scene = SCNScene(named: "art.scnassets/field.scn")
+        // シーンからノードを検索
+        let fieldNode = (scene?.rootNode.childNode(withName: "field", recursively: false))!
+        // 表示する座標を指定する
+        fieldNode.position = screenPos
+        // ノードの作成
+        sceneView.scene.rootNode.addChildNode(fieldNode)
     }
     
     // MARK: - ARSCNViewDelegate
