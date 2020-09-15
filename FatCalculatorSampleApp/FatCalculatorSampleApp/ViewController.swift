3cd8221869c4f56e6e8d01946a577332288b8841
Ohara <nori.tukareta@gmail.com>
2020-09-15 23:33:10 +0900

    MVC学習用の電卓アプリ作成

---
+++ FatCalculatorSampleApp/FatCalculatorSampleApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FatCalculatorSampleApp
+//
+//  Created by 尾原徳泰 on 2020/09/15.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // レイアウトもコードで生成
+    // ボタンの生成
+    // ラベル生成
+    
+    //　表示機能
+    // 数字入力機能
+    //　クリア機能
+
+}
+
