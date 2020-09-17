34ab860756c8696b6088650f4c46b59e7b715be8
Ohara <nori.tukareta@gmail.com>
2020-09-17 23:58:33 +0900

    buttonの座標計算はずべて成功

--- FatCalculatorSampleApp/FatCalculatorSampleApp/ViewController.swift
+++ FatCalculatorSampleApp/FatCalculatorSampleApp/ViewController.swift
@@ -53,47 +53,91 @@ class ViewController: UIViewController {
                 plsceNumberName = "1";
                 buttonPlaceX = buttonSizeWidth * 1;
                 buttonPlaceY = buttonSizeHight * 9
+                print("-------------------------------")
+                print("placeNumber:\(placeNumber)")
+                print("buttonPlaceX:\(buttonPlaceX)")
+                print("buttonPlaceY:\(buttonPlaceY)")
             case 2:
                 plsceNumberName = "2"
                 buttonPlaceX = buttonSizeWidth * 2;
                 buttonPlaceY = buttonSizeHight * 9
+                print("-------------------------------")
+                print("placeNumber:\(placeNumber)")
+                print("buttonPlaceX:\(buttonPlaceX)")
+                print("buttonPlaceY:\(buttonPlaceY)")
             case 3:
                 plsceNumberName = "3"
                 buttonPlaceX = buttonSizeWidth * 3;
                 buttonPlaceY = buttonSizeHight * 9
+                print("-------------------------------")
+                print("placeNumber:\(placeNumber)")
+                print("buttonPlaceX:\(buttonPlaceX)")
+                print("buttonPlaceY:\(buttonPlaceY)")
             case 4:
                 plsceNumberName = "4"
                 buttonPlaceX = buttonSizeWidth * 1;
                 buttonPlaceY = buttonSizeHight * 8
+                print("-------------------------------")
+                print("placeNumber:\(placeNumber)")
+                print("buttonPlaceX:\(buttonPlaceX)")
+                print("buttonPlaceY:\(buttonPlaceY)")
             case 5:
                 plsceNumberName = "5"
                 buttonPlaceX = buttonSizeWidth * 2;
                 buttonPlaceY = buttonSizeHight * 8
+                print("-------------------------------")
+                print("placeNumber:\(placeNumber)")
+                print("buttonPlaceX:\(buttonPlaceX)")
+                print("buttonPlaceY:\(buttonPlaceY)")
             case 6:
                 plsceNumberName = "6"
                 buttonPlaceX = buttonSizeWidth * 3;
                 buttonPlaceY = buttonSizeHight * 8
+                print("-------------------------------")
+                print("placeNumber:\(placeNumber)")
+                print("buttonPlaceX:\(buttonPlaceX)")
+                print("buttonPlaceY:\(buttonPlaceY)")
             case 7:
                 plsceNumberName = "7"
                 buttonPlaceX = buttonSizeWidth * 1;
                 buttonPlaceY = buttonSizeHight * 7
+                print("-------------------------------")
+                print("placeNumber:\(placeNumber)")
+                print("buttonPlaceX:\(buttonPlaceX)")
+                print("buttonPlaceY:\(buttonPlaceY)")
             case 8:
                 plsceNumberName = "8"
                 buttonPlaceX = buttonSizeWidth * 2;
                 buttonPlaceY = buttonSizeHight * 7
+                print("-------------------------------")
+                print("placeNumber:\(placeNumber)")
+                print("buttonPlaceX:\(buttonPlaceX)")
+                print("buttonPlaceY:\(buttonPlaceY)")
             case 9:
                 plsceNumberName = "9"
                 buttonPlaceX = buttonSizeWidth * 3;
                 buttonPlaceY = buttonSizeHight * 7
+                print("-------------------------------")
+                print("placeNumber:\(placeNumber)")
+                print("buttonPlaceX:\(buttonPlaceX)")
+                print("buttonPlaceY:\(buttonPlaceY)")
             case 0:
                 plsceNumberName = "0"
                 buttonPlaceX = buttonSizeWidth * 1;
                 buttonPlaceY = buttonSizeHight * 10
+                print("-------------------------------")
+                print("placeNumber:\(placeNumber)")
+                print("buttonPlaceX:\(buttonPlaceX)")
+                print("buttonPlaceY:\(buttonPlaceY)")
                 print("KO")
             case 10:
                 plsceNumberName = "00"
                 buttonPlaceX = buttonSizeWidth / 2 * 3;
                 buttonPlaceY = buttonSizeHight * 10
+                print("-------------------------------")
+                print("placeNumber:\(placeNumber)")
+                print("buttonPlaceX:\(buttonPlaceX)")
+                print("buttonPlaceY:\(buttonPlaceY)")
             default:
                 print("errror");
                 return
@@ -103,22 +147,22 @@ class ViewController: UIViewController {
                 // 0と10の場合
                 if let buttonPlaceX = buttonPlaceX, let buttonPlaceY = buttonPlaceY {
                     uiButton.frame = CGRect(x: 10, y: buttonPlaceY, width: buttonSizeWidth * 3 / 2, height: buttonSizeHight)
-                    
+                    print("$$$$$$$$$$$$$$$$$$$$$$$$$4")
                 }
-            } else if placeNumber == 5 {
+            } else {
                 // 1~9の場合
                 if let buttonPlaceX = buttonPlaceX, let buttonPlaceY = buttonPlaceY {
                     uiButton.frame = CGRect(x: 10, y: buttonPlaceY, width: buttonSizeWidth, height: buttonSizeHight)
-                    // buttonのラベル作成
-                    uiButton.setTitle(plsceNumberName, for: UIControl.State.normal)
-                    // buttonのフォントサイズを設定
-                    uiButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
-                    // タップされた時のアクション
-                    uiButton.addTarget(self, action: #selector(ViewController.buttonTapped(placeNumber:)), for: .touchUpOutside)
-                    uiButton.backgroundColor = UIColor.red
-                    // viewにボタンを追加
-                    self.view.addSubview(uiButton)
-                    print("ButtonMake")
+//                    // buttonのラベル作成
+//                    uiButton.setTitle(plsceNumberName, for: UIControl.State.normal)
+//                    // buttonのフォントサイズを設定
+//                    uiButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+//                    // タップされた時のアクション
+//                    uiButton.addTarget(self, action: #selector(ViewController.buttonTapped(placeNumber:)), for: .touchUpOutside)
+//                    uiButton.backgroundColor = UIColor.red
+//                    // viewにボタンを追加
+//                    self.view.addSubview(uiButton)
+//                    print("ButtonMake")
                 }
             }
             
