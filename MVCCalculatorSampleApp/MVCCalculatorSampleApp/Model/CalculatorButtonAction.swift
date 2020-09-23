9badc38f8541d2642730350c3736a3b48ed8434c
Ohara <nori.tukareta@gmail.com>
2020-09-23 23:45:37 +0900

    buttonの2度押しを阻止する関数実装

--- MVCCalculatorSampleApp/MVCCalculatorSampleApp/Model/CalculatorButtonAction.swift
+++ MVCCalculatorSampleApp/MVCCalculatorSampleApp/Model/CalculatorButtonAction.swift
@@ -6,5 +6,35 @@
 //  Copyright © 2020 尾原徳泰. All rights reserved.
 //
 
-// 四則演算用ボタンが押された時の挙動
+// 四則演算用ボタンが押された時の挙動担当
 import Foundation
+import UIKit
+
+class CalculatorButtonAction {
+    // 四則演算用ボタンの押された時のリターン
+    // 四則演算用ボタンの押された時のボタンの色変え:リターンなし
+    func changeCalculatorButtonColor(calculatorType:Int) {
+    // 入力されている文字数が規定以下なら
+    switch calculatorType {
+                case 1:
+                    additionButton.backgroundColor = UIColor.orange
+                case 2:
+                    subtractionButton.backgroundColor = UIColor.orange
+                case 3:
+                    divisionButton.backgroundColor = UIColor.orange
+                case 4:
+                    multiplicationButton.backgroundColor = UIColor.orange
+                default:
+                    print("error")
+                    return
+                }
+            
+            }
+    func banTapCalculatorButton(additionButton:UIButton, subtractionButton:UIButton, divisionButton:UIButton, multiplicationButton:UIButton) {
+        // ボタンを押せないように設定
+        additionButton.isEnabled = false
+        subtractionButton.isEnabled = false
+        divisionButton.isEnabled = false
+        multiplicationButton.isEnabled = false
+    }
+}
