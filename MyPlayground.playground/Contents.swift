630c5b52d319ea9451c9b27ec3405252471e18bf
Ohara <nori.tukareta@gmail.com>
2020-10-02 23:38:42 +0900

    get set　実装　実験開始

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,38 @@
 import UIKit
 
-class someClass {
-    var 
-}
+class Product {
+    var name:String // 商品名
+    var price:Int // 商品の値段
+    // 商品にかかる税金
+    var consumptionTax:Int {
+        get {
+            // 商品の値段から消費税を計算
+            print("Done Get")
+            return Int(Float(price) * 0.1) // 税率10％
+            
+        }
+        set {
+            // 消費税から値段を計算
+            price = Int(Float(newValue) / 0.1)
+            print("Done Set")
+        }
+    }
+        //商品名と商品の値段を初期化
+        init(name: String, price: Int) {
+            self.name = name
+            self.price = price
+        }
+    }
+
+// 100円のリンゴインスタンスを生成
+var apple = Product(name: "リンゴ", price: 100)
+
+// 100円のリンゴの消費税
+apple.consumptionTax
+
+apple.price = 200 //値段を変更
+apple.consumptionTax
+
+
+apple.consumptionTax = 30 // 税金を変更
+apple.price
