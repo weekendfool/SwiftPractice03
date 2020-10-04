7518b904841e4058d84acbad48f3f6aadfb5d83c
Ohara <nori.tukareta@gmail.com>
2020-10-04 16:36:30 +0900

    TaskCellファイル作成

---
+++ MVCSampleMakeApp/MVCSampleMakeApp/View/TaskCell.swift
@@ -0,0 +1,13 @@
+//
+//  TaskCell.swift
+//  MVCSampleMakeApp
+//
+//  Created by 尾原徳泰 on 2020/10/04.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+    
+}
