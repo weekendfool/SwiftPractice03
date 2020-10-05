7fa87e93d17b58cc08a3354f6ce553d2b8c7cf60
Ohara <nori.tukareta@gmail.com>
2020-10-06 03:03:17 +0900

    viewの描写部分実装分、一時保存

--- MVCSampleMakeApp/MVCSampleMakeApp/View/TaskCell.swift
+++ MVCSampleMakeApp/MVCSampleMakeApp/View/TaskCell.swift
@@ -16,11 +16,11 @@ class TaskCell: UITableViewCell {
     // 初期化してviewを描写
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        // UILabelのインスタンス生成
+        // taskLabelのインスタンス生成
         self.taskLabel = UILabel()
-        // UILabelの文字色変更
+        // taskLabelの文字色変更
         self.taskLabel.textColor = .black
-        // UILabelのフォント設定
+        // taskLabelのフォント設定
         self.taskLabel.font = UIFont.systemFont(ofSize: 14)
         // viewに追加
         contentView.addSubview(self.taskLabel)
@@ -40,4 +40,30 @@ class TaskCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // taskLabelの位置を設定
+        self.taskLabel.frame = CGRect(x: 15.0, y: 15.0, width: self.contentView.frame.width - 30, height: 15.0)
+        self.deadlineLabel.frame = CGRect(x: self.taskLabel.frame.origin.x, y: self.taskLabel.frame.maxY + 8, width: self.taskLabel.frame.width, height: 15.0)
+    }
+    // コンピューティッドプロパティの宣言
+    var task: Task? {
+        didSet {
+            //　taskがnilでない時の挙動
+            guard let t = task else { return }
+            // taskLabelにtextを反映する
+            self.taskLabel.text = t.text
+            
+            // DateFormatterのインスタンス作成
+            let formatter = DateFormatter()
+            // 日付のフォーマットを決定
+            formatter.dateFormat = "yyyy/MM/dd"
+            // deadlineLabelに設定したフォーマットで日付を代入
+            self.deadlineLabel.text = formatter.string(from: t.deadline)
+            
+        }
+    }
+    
+
 }
