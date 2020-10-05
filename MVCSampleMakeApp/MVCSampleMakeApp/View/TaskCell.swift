cb96c86d5a6608105e3ef527285741fcf4b7f19a
Ohara <nori.tukareta@gmail.com>
2020-10-06 02:38:53 +0900

    labelの設定処理実装

--- MVCSampleMakeApp/MVCSampleMakeApp/View/TaskCell.swift
+++ MVCSampleMakeApp/MVCSampleMakeApp/View/TaskCell.swift
@@ -16,5 +16,28 @@ class TaskCell: UITableViewCell {
     // 初期化してviewを描写
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        // UILabelのインスタンス生成
+        self.taskLabel = UILabel()
+        // UILabelの文字色変更
+        self.taskLabel.textColor = .black
+        // UILabelのフォント設定
+        self.taskLabel.font = UIFont.systemFont(ofSize: 14)
+        // viewに追加
+        contentView.addSubview(self.taskLabel)
+        
+        // deadlineLabelのインスタンス生成
+        self.deadlineLabel = UILabel()
+        // deadlineLabelの文字色変更
+        self.deadlineLabel.textColor = .black
+        // deadlineLabelのフォント設定
+        self.deadlineLabel.font = UIFont.systemFont(ofSize: 14)
+        // viewに追加
+        contentView.addSubview(self.deadlineLabel)
     }
+    
+    // エラー処理
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
