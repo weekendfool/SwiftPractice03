0570481efaa37e7925322d0ce96dfc050dc2c490
Ohara <nori.tukareta@gmail.com>
2020-10-07 23:39:40 +0900

    UIパーツを配置

--- MVCSampleMakeApp/MVCSampleMakeApp/View/CreateTaskView.swift
+++ MVCSampleMakeApp/MVCSampleMakeApp/View/CreateTaskView.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2020 尾原徳泰. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol CreateTaskViewDelegate: class {
+    func createView(taskEditting view: CreateTaskView, text: String)
+    func createView(deadlineEditting View: CreateTaskView, deadline: Date)
+    func createView(saveButtonDidTap view: CreateTaskView)
+}
+
+class CreateTaskView: UIView {
+    
+    private var taskTextField: UITextField!
+    private var datePicker: UIDatePicker!
+    private var deadLineTextField: UITextField!
+    private var saveButton: UIButton!
+
+}
