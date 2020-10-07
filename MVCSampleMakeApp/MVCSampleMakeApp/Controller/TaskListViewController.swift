9d8d48cfe6b667ea73a24430ad46c4fc7f62eeb9
Ohara <nori.tukareta@gmail.com>
2020-10-08 03:03:14 +0900

    一時保存

---
+++ MVCSampleMakeApp/MVCSampleMakeApp/Controller/TaskListViewController.swift
@@ -0,0 +1,50 @@
+//
+//  TaskListViewController.swift
+//  MVCSampleMakeApp
+//
+//  Created by 尾原徳泰 on 2020/10/08.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController {
+    
+    var dataSource: TaskDataManager!
+    var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.dataSource = TaskDataManager()
+        // tableViewの設定と追加
+        self.tableView = UITableView(frame: self.view.bounds, style: .plain)
+        self.tableView.register(TaskCell.self, forCellReuseIdentifier: "Cell")
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.view.addSubview(self.tableView)
+        
+        let barButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(barButtonTapped(_:)))
+        self.navigationItem.rightBarButtonItem = barButton
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // この画面を開くたびに情報を更新して反映
+        self.dataSource.loadData()
+        self.tableView.reloadData()
+    }
+    
+    @objc func barButtonTapped(_ sender: UIBarButtonItem) {
+        // タスク作成画面への遷移コード
+        let controller = UINavigationController()
+        let navi = UINavigationController(rootViewController: controller)
+        self.present(navi, animated: true, completion:  nil)
+    }
+    
+    extension TaskListViewController: UITableViewDataSource {
+        
+        func tableView(_ tableView:UITableView, numberOfRowsInSection)
+        
+    }
+}
