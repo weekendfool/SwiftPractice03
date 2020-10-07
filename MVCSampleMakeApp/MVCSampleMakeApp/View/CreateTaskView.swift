f32d53f4c372c606f195bdb37b817ae502b554d9
Ohara <nori.tukareta@gmail.com>
2020-10-07 23:56:23 +0900

    各UIパーツのインスタンス生成や設定実装

--- MVCSampleMakeApp/MVCSampleMakeApp/View/CreateTaskView.swift
+++ MVCSampleMakeApp/MVCSampleMakeApp/View/CreateTaskView.swift
@@ -16,9 +16,48 @@ protocol CreateTaskViewDelegate: class {
 
 class CreateTaskView: UIView {
     
+    // UIパーツの宣言
     private var taskTextField: UITextField!
     private var datePicker: UIDatePicker!
     private var deadLineTextField: UITextField!
     private var saveButton: UIButton!
+    
+    // デリゲートの設定
+    weak var delegate: CreateTaskViewDelegate?
+    
+    // 初期化の実行
+    required override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // インスタンスを生成
+        self.taskTextField = UITextField()
+        // delegateの対象を地震に設定
+        self.taskTextField.delegate = self
+        self.taskTextField.tag = 0
+        self.taskTextField.placeholder = "予定を入れてください"
+        // viewに追加
+        self.addSubview(self.taskTextField)
+        
+        // インスタンスを生成
+        self.deadLineTextField = UITextField()
+        // delegateの対象を地震に設定
+        self.deadLineTextField.delegate = self
+        self.deadLineTextField.tag = 1
+        self.deadLineTextField.placeholder = "期限を設定してください"
+        // viewに追加
+        self.addSubview(self.deadLineTextField)
+        
+        // インスタンスを生成
+        self.datePicker = UIDatePicker()
+        self.datePicker.datePickerMode = .dateAndTime
+        self.datePicker.addTarget(self, action: #selector(datePickerValueChanged(_:)), for: .touchUpInside)
+        
+        // deadLineTextFieldが選択されたとにきピッカーを表示する
+        self.deadLineTextField.inputView = self.datePicker
+        
+        // インスタンスを生成
+        self.saveButton = UIButton()
+    }
 
+    
 }
